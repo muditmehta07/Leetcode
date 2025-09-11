1c392a8a41c9f4da6b32a97900e91698bd2e93e7
Mudit Mehta <70369287+muditmehta07@users.noreply.github.com>
2025-09-11 23:08:16 +0530

    Create TwoSum.py

---
+++ TwoSum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        L = {}
+
+        for i, num in enumerate(nums):
+            sub = target - num
+            
+            if sub in L:
+                return [L[sub], i]
+
+            L[num] = i
